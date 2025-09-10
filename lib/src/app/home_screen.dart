a3b6b3579d3ea2f39aa4140f4c49562241b3cbd0
Aniakii <prezes.kot.ani@gmail.com>
2025-09-10 13:55:09 +0200

    change const text to appLocalization text

--- lib/src/app/home_screen.dart
+++ lib/src/app/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_kitchen/src/core/routing/app_router.dart';
+import '../core/l10n/app_localizations.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -9,17 +10,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
-      appBarBuilder: (context, router) =>
-          AppBar(title: Center(child: Text("Smart Kitchen"))),
+      appBarBuilder: (context, router) => AppBar(
+        title: Center(child: Text(AppLocalizations.of(context)!.appName)),
+      ),
       routes: [KitchenRoute(), RecipesRoute()],
       bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
         currentIndex: tabsRouter.activeIndex,
         onTap: tabsRouter.setActiveIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Kitchen'),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: AppLocalizations.of(context)!.kitchenTitle,
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.menu_book),
-            label: 'Recipes',
+            label: AppLocalizations.of(context)!.recipesTitle,
           ),
         ],
       ),
